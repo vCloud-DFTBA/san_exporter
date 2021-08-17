c0c1a31ad2a0510b04ecfa50067c4d3f930e9ef1
trangtran <43900034+trangtran871997@users.noreply.github.com>
2021-08-17 20:05:38 +0700

    Netapp update (#15)
    * Refactor Netapp driver

--- san_exporter/drivers/netapp/main.py
+++ san_exporter/drivers/netapp/main.py
@@ -25,7 +25,6 @@ from san_exporter.utils.utils import cache_data
 
 
 class NetAppExporter(base_driver.ExporterDriver):
-
     def __init__(self, config=None, interval=10):
         super().__init__(config, interval)
         self.netapp_api_ip = config['netapp_api_ip']
@@ -33,109 +32,84 @@ class NetAppExporter(base_driver.ExporterDriver):
         self.netapp_username = config['netapp_username']
         self.netapp_password = config['netapp_password']
         self.backend_name = config['name']
-        self.baseURL = 'https://' + self.netapp_api_ip + '/api'
         self.auth = (self.netapp_username, self.netapp_password)
-        self.headers = {'Accept': 'application/json',
-                        'Content-Type': 'application/json'}
+        self.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
     def get_cluster_metrics(self):
-        response = requests.get(self.baseURL + '/cluster',
-                                headers=self.headers, auth=self.auth,
+        cluster_data = []
+        response = requests.get('https://' + self.netapp_api_ip + '/api/cluster', headers=self.headers, auth=self.auth,
                                 verify=False).json()
-        cluster_data = {
-            'name': response['name'],
-            'version': response['version']['full'],
-            'read_iops': response['metric']['iops']['read'],
-            'write_iops': response['metric']['iops']['write'],
-            'other_iops': response['metric']['iops']['other'],
-            'read_latency': response['metric']['latency']['read'],
-            'write_latency': response['metric']['latency']['write'],
-            'other_latency': response['metric']['latency']['other'],
-            'read_throughput': response['metric']['throughput']['read'],
-            'write_throughput': response['metric']['throughput']['write'],
-            'other_throughput': response['metric']['throughput']['other'],
-            'status': response['metric']['status']}
+        cluster_metric = {'name': response['name'], 'version': response['version']['full'],
+                          'read_iops': response['metric']['iops']['read'],
+                          'write_iops': response['metric']['iops']['write'],
+                          'other_iops': response['metric']['iops']['other'],
+                          'read_latency': response['metric']['latency']['read'],
+                          'write_latency': response['metric']['latency']['write'],
+                          'other_latency': response['metric']['latency']['other'],
+                          'read_throughput': response['metric']['throughput']['read'],
+                          'write_throughput': response['metric']['throughput']['write'],
+                          'other_throughput': response['metric']['throughput']['other'],
+                          'status': response['metric']['status']
+                          }
+        cluster_metric.update({'san_ip': self.netapp_api_ip})
+        cluster_data.append(cluster_metric)
         return cluster_data
 
     def get_node_info(self):
-        response = requests.get(self.baseURL + '/cluster/nodes?fields=name,'
-                                               'serial_number,'
-                                               'state,model,'
-                                               'version',
-                                headers=self.headers, auth=self.auth,
-                                verify=False).json()
         node_data = []
+        response = requests.get(
+            'https://' + self.netapp_api_ip + '/api/cluster/nodes?fields=name,serial_number,state,model,version',
+            headers=self.headers, auth=self.auth, verify=False).json()
         for t in response['records']:
-            data = {
-                'name': t['name'],
-                'state': t['state'],
-                'model': t['model'],
-                'serial_number': t['serial_number'],
-                'version': t['version']['full'],
-            }
+            data = {'name': t['name'], 'state': t['state'], 'model': t['model'], 'serial_number': t['serial_number'],
+                    'version': t['version']['full']}
+            data.update({'san_ip': self.netapp_api_ip})
             node_data.append(data)
         return node_data
 
     def get_pool_info(self):
-        response = requests.get(
-            self.baseURL + '/storage/volumes?fields=metric,'
-                           'state,space',
-            headers=self.headers,
-            auth=self.auth,
-            verify=False).json()
         pool_data = []
+        response = pool_info = requests.get(
+            'https://' + self.netapp_api_ip + "/api/storage/volumes?fields=metric,state,space", headers=self.headers,
+            auth=self.auth, verify=False).json()
         for t in response['records']:
             if t['name'].startswith('agg'):
-                data = {
-                    'name': t['name'],
-                    'size_total': t['space']['available'],
-                    'size_used': t['space']['used'],
-                    'read_iops': t['metric']['iops']['read'],
-                    'write_iops': t['metric']['iops']['write'],
-                    'other_iops': t['metric']['iops']['other'],
-                    'read_latency': t['metric']['latency']['read'],
-                    'write_latency': t['metric']['latency']['write'],
-                    'other_latency': t['metric']['latency']['other'],
-                    'read_throughput': t['metric']['throughput']['read'],
-                    'write_throughput': t['metric']['throughput']['write'],
-                    'other_throughput': t['metric']['throughput']['other'],
-                    'status': t['metric']['status']}
+                data = {'name': t['name'], 'size_total': t['space']['available'], 'size_used': t['space']['used'],
+                        'read_iops': t['metric']['iops']['read'], 'write_iops': t['metric']['iops']['write'],
+                        'other_iops': t['metric']['iops']['other'], 'read_latency': t['metric']['latency']['read'],
+                        'write_latency': t['metric']['latency']['write'],
+                        'other_latency': t['metric']['latency']['other'],
+                        'read_throughput': t['metric']['throughput']['read'],
+                        'write_throughput': t['metric']['throughput']['write'],
+                        'other_throughput': t['metric']['throughput']['other'], 'status': t['metric']['status']}
+                data.update({'san_ip': self.netapp_api_ip})
                 pool_data.append(data)
         return pool_data
 
     def get_disk_info(self):
-        response = requests.get(
-            self.baseURL + '/storage/disks?fields=name,'
-                           'state,model,serial_number',
-            headers=self.headers, auth=self.auth,
-            verify=False).json()
         disk_data = []
+        response = requests.get(
+            'https://' + self.netapp_api_ip + '/api/storage/disks?fields=name,state,model,serial_number',
+            headers=self.headers, auth=self.auth, verify=False).json()
         for t in response['records']:
-            data = {
-                'name': t['name'],
-                'state': t['state'],
-                'model': t['model'],
-                'serial_number': t['serial_number'],
-            }
+            data = {'name': t['name'], 'state': t['state'], 'model': t['model'], 'serial_number': t['serial_number']}
+            data.update({'san_ip': self.netapp_api_ip})
             disk_data.append(data)
         return disk_data
 
     def run(self):
         while True:
             data = {}
-            try:
-                data['cluster'] = self.get_cluster_metrics()
-                data['node'] = self.get_node_info()
-                data['pool'] = self.get_pool_info()
-                data['disk'] = self.get_disk_info()
-                cache_data(self.cache_file, data)
-            except:
-                sleep(sleep.interval)
-                continue
+            data['cluster'] = self.get_cluster_metrics()
+            data['node'] = self.get_node_info()
+            data['pool'] = self.get_pool_info()
+            data['disk'] = self.get_disk_info()
+            cache_data(self.cache_file, data)
+            sleep(self.interval)
 
 
 def main(config, interval):
     netapp_metrics = prometheus_metrics.NetAppMetrics(config=config)
     netapp_exporter = NetAppExporter(config, interval)
     netapp_exporter.start()
-    return (netapp_exporter, netapp_metrics)
+    return netapp_exporter, netapp_metrics
