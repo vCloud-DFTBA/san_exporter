4d7550ab354b181a85cdd585af893ac0464e4154
trangtran <43900034+trangtran871997@users.noreply.github.com>
2021-08-17 18:28:29 +0700

    Add Driver Netapp

--- san_exporter/drivers/netapp/main.py
+++ san_exporter/drivers/netapp/main.py
@@ -35,7 +35,7 @@ class NetAppExporter(base_driver.ExporterDriver):
         self.auth = (self.netapp_username, self.netapp_password)
         self.headers = {
             'Accept': 'application/json',
-            "Content-Type": "application/json"}
+            'Content-Type': 'application/json'}
 
     def get_cluster_metrics(self):
         cluster_data = []
